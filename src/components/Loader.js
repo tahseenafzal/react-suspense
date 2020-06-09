b5ce7bb4e9f8c36aa6a37013e6e71fbdd4d9ead1
Tehseen Afzal <abuashja@gmail.com>
2020-06-09 23:02:13 +0500

    Add CircularProgress of Material ui

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { CircularProgress } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+}));
 
 const Loader = () => {
-  return <h1>Loading...</h1>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CircularProgress />
+    </div>
+  );
 };
 
 export default Loader;
